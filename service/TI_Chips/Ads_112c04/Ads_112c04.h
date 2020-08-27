2f60f4fec086ba4b16ecde387f428843a19e7064
ActivePeter <1020401660@qq.com>
2020-08-28 03:48:20 +0800

    s

---
+++ service/TI_Chips/Ads_112c04/Ads_112c04.h
@@ -0,0 +1,50 @@
+#ifndef __Ads_112c04_H__
+#define __Ads_112c04_H__
+extern "C" {
+#include "../../../drv/pa_CommonDrv.h"
+
+}
+
+class Ads_112c04
+{
+public:
+	Ads_112c04();
+	static Ads_112c04 instance;
+	void init(unsigned char A0, unsigned char A1);
+	void selectChannel(unsigned char channel, unsigned char gain, unsigned char mode);
+	double readADC();
+	void reset();
+	//引脚状态
+	static const unsigned char DGND = 0;
+	static const unsigned char DVDD = 1;
+	static const unsigned char SDA = 2;
+	static const unsigned char SCL = 3;
+
+	static const unsigned char CHANNEL_0 = 0;
+	static const unsigned char CHANNEL_1 = 1;
+	static const unsigned char CHANNEL_2 = 2;
+
+	static const unsigned char GAIN_1 = 0;
+	static const unsigned char GAIN_2 = 1;
+	static const unsigned char GAIN_4 = 2;
+	static const unsigned char GAIN_8 = 3;
+	static const unsigned char GAIN_16 = 4;
+	static const unsigned char GAIN_32 = 5;
+	static const unsigned char GAIN_64 = 6;
+	static const unsigned char GAIN_128 = 7;
+
+	/*void setKeypadCallback(void (*keypadCallback)(unsigned char x, unsigned char y));*/
+private:
+	static const unsigned char BIT_ST_DRDY = 7; //data ready
+	static const unsigned char BIT_INP1 = 6; //input channel select
+	static const unsigned char BIT_INP0 = 5; //input channel select
+	static const unsigned char BIT_SC = 4; //conversion mode (single/continuous)
+	static const unsigned char BIT_DR1 = 3; //data rate
+	static const unsigned char BIT_DR0 = 2; //data rate
+	static const unsigned char BIT_PGA1 = 1; //gain
+	static const unsigned char BIT_PGA0 = 0; //gain
+
+	char I2C_ADDRESS;
+};
+
+#endif
