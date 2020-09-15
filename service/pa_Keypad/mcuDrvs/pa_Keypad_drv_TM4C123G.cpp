01860e317ee48907d064a67cc5ce3510e2c1e9f2
ActivePeter <1020401660@qq.com>
2020-09-15 12:33:08 +0800

    键盘初始化bug

--- service/pa_Keypad/mcuDrvs/pa_Keypad_drv_TM4C123G.cpp
+++ service/pa_Keypad/mcuDrvs/pa_Keypad_drv_TM4C123G.cpp
@@ -98,8 +98,6 @@ void pa_Keypad::initInput()
         uint32_t pin=pa_Keypad_getInputPortOrPinByIndex(i,GpioPortOrPin_Pin);
         uint32_t port=pa_Keypad_getInputPortOrPinByIndex(i,GpioPortOrPin_Port);
         GPIOPinTypeGPIOInput(port,pin);
-        // GPIOPUR
-                     
     }
 }
 void pa_Keypad::initOutput()
@@ -108,8 +106,7 @@ void pa_Keypad::initOutput()
     {
         uint32_t pin=pa_Keypad_getOutputPortOrPinByIndex(i,GpioPortOrPin_Pin);
         uint32_t port=pa_Keypad_getOutputPortOrPinByIndex(i,GpioPortOrPin_Port);
-        GPIOPinTypeGPIOInput(port,pin);
-
+        GPIOPinTypeGPIOOutput(port,pin);
     }
 }
 
