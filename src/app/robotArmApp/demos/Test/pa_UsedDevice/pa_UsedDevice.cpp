fa2da7a2e7b1f8d467ce3dac6017ee4269cf5005
ActivePeter <1020401660@qq.com>
2020-12-04 22:47:06 +0800

    p

--- src/app/robotArmApp/demos/Test/pa_UsedDevice/pa_UsedDevice.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "pa_UsedDevice.h"
-#ifdef RobotArmApp_demo_Test
-namespace pa_UsedDevice
-{
-    pa_ILI9341 &ili9341 = pa_ILI9341::instance;
-    pa_touchScreen &touch = pa_touchScreen::instance;
-} // namespace pa_UsedDevice
-#endif
