75c4a9231118708107a67e909c4b7b70b71d5003
ActivePeter <1020401660@qq.com>
2020-12-28 12:31:54 +0800

    send point arr is ok

---
+++ src/util/transmitData/transmitData.h
@@ -0,0 +1,9 @@
+typedef union
+{
+    float data_float;
+    int data_int;
+    char data_char[4];
+    unsigned char data_uchar[4];
+    short data_short[2];
+    unsigned short data_ushort[2];
+} TransmitData_4byte;
